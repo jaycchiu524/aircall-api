11cd27fe968f6427ea1b845f38c3cd7b875cfded
Jay Chiu <jaycchiu524@gmail.com>
2022-09-27 00:14:22 -0400

    fix: add node-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports= {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.node$/,
+        use: "node-loader",
       }
     ]
   },
